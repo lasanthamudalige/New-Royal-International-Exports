c5323c2fc53b3a007c0b3d39368664df2e567eba
lasantha kumara <91461938+lasantha-kumara@users.noreply.github.com>
2022-07-05 13:08:57 +0530

    Add files via upload

---
+++ contactform.php
@@ -0,0 +1,16 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $subject = $_POST['subject'];
+    $phone_number = $_POST['number'];
+    $message = $_POST['message'];
+
+    $mailTo = "mail@newroyalinternationalexporters.com";
+    $headers = "From :" . $mailFrom;
+    $txt = "You have received and e-mail from " . $name . "\n\nsubject = " . $subject . "\nphone number = " . $phone_number . "\nmessage = " . $message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.html?mailsend");
+}
